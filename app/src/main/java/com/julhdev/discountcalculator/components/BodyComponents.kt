aa09fe6773a79f75f6a4b9552f5efa44590d62e3
Julieth Sarmiento <jasa1999@hotmail.com>
2025-09-02 11:19:08 -0500

    feat: se trabajo en el header y los spacers genericos

--- app/src/main/java/com/julhdev/discountcalculator/components/BodyComponents.kt
+++ app/src/main/java/com/julhdev/discountcalculator/components/BodyComponents.kt
@@ -1,2 +1,30 @@
 package com.julhdev.discountcalculator.components
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.*
+
+@Composable
+        /**
+         * Componente para agregar espacio vertical
+         * @param size El tamaño del espacio vertical (por defecto es 10.dp)
+         * usage: SpaceHeight(20.dp) o SpaceHeight() para el valor por defecto
+         */
+fun SpaceHeight(size: Dp = 10.dp) {
+  Spacer(modifier = Modifier
+    .height(size)
+  )
+}
+
+@Composable
+        /**
+         * Componente para agregar espacio horizontal
+         * @param size El tamaño del espacio horizontal (por defecto es 10.dp)
+         * usage: SpaceWidth(20.dp) o SpaceWidth() para el valor por defecto
+         */
+fun SpaceWidth(size: Dp = 10.dp) {
+  Spacer(modifier = Modifier
+    .width(size)
+  )
+}
--- app/src/main/java/com/julhdev/discountcalculator/views/HomeView.kt
+++ app/src/main/java/com/julhdev/discountcalculator/views/HomeView.kt
@@ -6,8 +6,11 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -19,6 +22,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.julhdev.discountcalculator.R
+import com.julhdev.discountcalculator.components.SpaceHeight
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -30,7 +34,14 @@ fun HomeView() {
         colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
           containerColor = MaterialTheme.colorScheme.secondary,
           titleContentColor = MaterialTheme.colorScheme.primary
-        )
+        ),
+        navigationIcon = {
+          Icon(
+            imageVector = Icons.Filled.AccountCircle,
+            contentDescription = "User",
+            tint = MaterialTheme.colorScheme.primary,
+          )
+        }
       )
     }
   ) {
@@ -43,15 +54,19 @@ fun HomeView() {
 fun ContentHomeView(paddingValues: PaddingValues ) {
   Column(
     modifier = Modifier
+      .fillMaxSize()
       .padding(paddingValues)
       .padding(10.dp),
     verticalArrangement = Arrangement.Center,
     horizontalAlignment = Alignment.CenterHorizontally
   ) {
     Image(
-      modifier = Modifier.fillMaxSize(),
       painter = painterResource(id = R.drawable.image),
-      contentDescription = "Background"
+      contentDescription = "Discount",
+      modifier = Modifier
+        .padding(10.dp)
     )
+    SpaceHeight(50.dp)
+    Text("Holis", color = MaterialTheme.colorScheme.secondary)
   }
 }
